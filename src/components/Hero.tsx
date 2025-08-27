f5ce244958bf5be7d1c3c5769abd974d668ea2ad
Aysla Loureiro <ayslaloureiro@gmail.com>
2025-08-27 20:55:00 +0100

    feat(hero): improve responsive layout for hero-content

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React from "react";
 
 interface HeroProps {
   imageSrc: string;
@@ -8,25 +7,31 @@ interface HeroProps {
 
 export const Hero = ({ imageSrc, imageAlt = "Imagem hero" }: HeroProps) => {
   return (
-    <section 
+    <section
       id="hero"
       className="h-screen pt-[64px] bg-cover bg-center md:bg-top text-white flex items-end md:items-center justify-center md:justify-start p-4 md:pl-[50px] lg:pl-[100px]"
       style={{ backgroundImage: `url(${imageSrc})` }}
-    >      
-      <div 
-        className="bg-[#3F1F56] bg-opacity-80 w-full max-w-[600px] px-5 py-7 sm:px-10 sm:py-14 rounded-2xl"
+    >
+      <div
+        className="bg-[#3F1F56] bg-opacity-80  md:w-[350px] lg:w-[500px] 2xl:w-[648px] lg:m-[-60px]  px-5 py-7 sm:px-5 sm:py-14 rounded-2xl "
         id="hero-content"
       >
-        <h2 className="text-2xl font-medium font-roboto lg:text-4xl sm:text-4xl" id="hero-title">
+        <h2
+          className="text-2xl font-medium font-roboto lg:text-4xl sm:text-4xl"
+          id="hero-title"
+        >
           Transforme sua carreira. <br />
           Migre para a tecnologia com suporte real e personalizado.
         </h2>
-        <h3 className="pt-4 font-roboto lg:text-3xl/none sm:text-2xl/snug text-xl/snug" id="hero-subtitle">
+        <h3
+          className="pt-4 font-roboto lg:text-3xl/none sm:text-2xl/snug text-xl/snug"
+          id="hero-subtitle"
+        >
           Se reinvente com aprendizado ágil e mentorias focadas na sua entrada
           no mercado de TI.
         </h3>
-        <div className="flex flex-col pt-6 md:flex-row gap-4" id="hero-buttons">
-          <button 
+        <div className="flex flex-col gap-4 pt-6 md:flex-row" id="hero-buttons">
+          <button
             className="bg-[#FFD700] text-black rounded-lg py-2 px-6 font-inter font-semibold hover:bg-[#FFC000] transition-colors"
             id="btn-cadastro"
           >
